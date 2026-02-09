63a49a9a2907348748983e45803c6f0a1d49b55f
MAOZ <maooz4426@gmail.com>
2026-02-09 09:18:43 +0900

    first

---
+++ greeting/cmd/client.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	pb "github.com/maooz4426/grpc-playground/greeting/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+func main() {
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewGreetingServiceClient(conn)
+
+	callSayHello(client, "Taro")
+}
+
+func callSayHello(client pb.GreetingServiceClient, name string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	req := &pb.HelloRequest{
+		Name: name,
+	}
+
+	res, err := client.SayHello(ctx, req)
+	if err != nil {
+		log.Fatalf("Error calling SayHello: %v", err)
+	}
+	log.Printf("Response: %s", res.GetMessage())
+}
---
+++ greeting/cmd/server.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "github.com/maooz4426/grpc-playground/greeting/proto"
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	pb.UnimplementedGreetingServiceServer
+}
+
+func (s *server) SayHello(ctx context.Context, req *pb.HelloRequest) (*pb.HelloResponce, error) {
+	log.Printf("Received: %v", req.GetName())
+
+	message := fmt.Sprintf("Hello, %s! Welcome to gRPC!", req.GetName())
+
+	return &pb.HelloResponce{
+		Message: message,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	pb.RegisterGreetingServiceServer(s, &server{})
+
+	log.Println("Server is running on port 50051...")
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to serve: %v", err)
+	}
+}
